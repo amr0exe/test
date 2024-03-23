b1bb9b9039fea82b99bd877c64b08e2a4a9468b4
amr0exe <zyklen908@gmail.com>
2024-03-23 17:24:20 +0545

    don't know

--- authHandle/src/Pages/ProtectedPage.jsx
+++ authHandle/src/Pages/ProtectedPage.jsx
@@ -6,6 +6,7 @@ export default function ProtectedPage() {
    if(!token) {
       return <Navigate to={"/signup"} />;
    }
+   
    return <div className="bg-zinc-800 h-screen">
       <div className="bg-zinc-950 p-2 flex justify-between items-center">
          <h1 className="ml-20 text-slate-200  font-sans">protectedPage</h1>
